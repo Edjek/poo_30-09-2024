f3a51833d6aa6b7edabb8ed471db04c81b3a2cb3
Edjek <edjek@hotmail.fr>
2024-09-30 15:26:01 +0200

    first commit

---
+++ 02-visibility-getter-setter-constructor/index.php
@@ -0,0 +1,8 @@
+<!-- Creer une class Car -->
+    <!-- propriete -->
+        <!-- model -->
+        <!-- color -->
+        <!-- year_release -->
+    
+    <!-- methode displayInformation -->
+        <!-- retourner "Ce véhicule est une XXX de XXX ans et sa couleur est XXX  -->
