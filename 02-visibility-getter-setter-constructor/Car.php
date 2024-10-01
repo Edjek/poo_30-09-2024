7893208fc5a6ba48ec6108a8569886efc8f4f41b
Edjek <edjek@hotmail.fr>
2024-10-01 09:12:30 +0200

    second day

--- 02-visibility-getter-setter-constructor/Car.php
+++ 02-visibility-getter-setter-constructor/Car.php
@@ -1,6 +1,15 @@
 <?php
 
+// VISIBILITE (public, private, protected)
+//    - public : accessible depuis l'exterieur et l'interieur de la classe.
+//    - private : accessible uniquement depuis l'interieur de la classe.
 
+// Encapsulation est le fait de rendre privé les propriétés ou les methodes d'une classe
+// pour empecher l'exterieur de modifier ou de lire les propriétés d'une classe.
+// Pour accéder aux propriétés privées d'une classe, on utilise les getters et les setters.
+
+// Un getter (accesseur) est une methode qui permet de récupérer la valeur d'une propriété privée.
+// Un setter (mutateur) est une méthode qui permet de modifier la valeur d'une propriété privée.
 class Car
 {
     private string $model;
@@ -17,6 +26,32 @@ class Car
     public function setModel($model)
     {
         $this->model = $model;
+
+        return $this;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getYearRelease(): int
+    {
+        return $this->yearRelease;
+    }
+
+    public function setYearRelease($year)
+    {
+        $this->yearRelease = $year;
+
+        return $this;
     }
 
     public function displayInformation(): string
--- 02-visibility-getter-setter-constructor/index.php
+++ 02-visibility-getter-setter-constructor/index.php
@@ -2,8 +2,12 @@
 
 include_once './Car.php';
 
+// - new est mot clé permettant d'effectuer une instanciation.
+// - Une class peut produire plusieurs objets. Nous pouvons donc effectuer plusieurs instanciations 'new'
+
 $car = new Car();
 
-$car->setModel('Audi');
-echo $car->getModel();
+$car->setModel('Audi')->setColor('bleue')->setYearRelease(2004);
 
+$car->setYearRelease(2004);
+echo $car->displayInformation();
