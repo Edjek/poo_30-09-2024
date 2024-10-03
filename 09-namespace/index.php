ce799b09e02b79bc25b16876fd86a63a1e1ebadf
Edjek <edjek@hotmail.fr>
2024-10-03 15:41:57 +0200

    fourth day

--- 09-namespace/index.php
+++ 09-namespace/index.php
@@ -1,7 +1,11 @@
 <?php
 
-include_once './Product.php';
+include_once './src/Entity/Product.php';
+
+use App\Entity\Product;
+
 
 $grillePain = new Product('grille pain Tefal', 'blabla');
 
 echo $grillePain->getDescription();
+echo Product::class;
