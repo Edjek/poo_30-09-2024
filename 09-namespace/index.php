810b852332b180a171857ebbac5c026138507e16
Edjek <edjek@hotmail.fr>
2024-10-04 10:37:34 +0200

    sixth day

--- 09-namespace/index.php
+++ 09-namespace/index.php
@@ -1,13 +1,27 @@
 <?php
 
-include_once './src/Entity/Product.php';
+use App\Entity\Peugeot;
+use App\Entity\Renault;
+use App\Entity\User;
 
-use App\Entity\Product;
+include_once './src/Abstract/AbstractVehicule.php';
+include_once './src/Interface/EngineInterface.php';
+include_once './src/Entity/Peugeot.php';
+include_once './src/Entity/Renault.php';
+include_once './src/Entity/User.php';
+
+$peugeot404 = new Peugeot();
+$renaultSCenic = new Renault();
+$rachid = new User();
+$rachid->setPseudo('Rachid EDJEKOUANE');
 
 
-$grillePain = new Product('grille pain Tefal', 'blabla');
+echo $peugeot404->boot() . '<br>';
+echo $renaultSCenic->boot() . '<br>';
 
-echo $grillePain->getDescription();
+echo $peugeot404->start($rachid);
+
+echo $renaultSCenic->start($rachid);
 
 // Product::class est une constante magique qui retourne le namespace de la classe
-echo Product::class;
+// echo Product::class;
